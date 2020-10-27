625b5da54632d88333875427fa98bd0a311ab93c
Neruthes <i@neruthes.xyz>
2020-10-27 15:26:11 +0000

    Regular update 2020-10-27T15:08:39-00:00

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 
 # Run this script as root to deploy os-install on a system image
 
-apt install git
+# apt install git
 cd /root/
-rm -r .os-install os-install
-git clone https://github.com/neruthes/os-install
-mv os-install .os-install
-source /root/.os-install/src/stage2.sh
+rm -r .os-install os-install 2> /dev/null
+mkdir .os-install
+cd .os-install
+
+# git clone https://github.com/neruthes/os-install
+OSIVER="v0.1.0"
+wget "https://github.com/neruthes/os-install/archive/$OSIVER.tar.gz"
+tar -pxf *.tar.gz
+mv os-install*/* .
+sh /root/.os-install/src/stage2.sh
