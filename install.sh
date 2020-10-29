9aa99f007ddb486b703d6f8e2e4d2e0ee7c17de5
Neruthes <i@neruthes.xyz>
2020-10-29 03:53:08 +0000

    Regular update 2020-10-29T02:38:12-00:00

--- install.sh
+++ install.sh
@@ -2,26 +2,29 @@
 
 # Run this script as root to deploy os-install on a system image
 
-# apt install git
+isERR=1
 cd /root/
 rm -r .os-install os-install 2> /dev/null
 mkdir .os-install
 cd .os-install
 
-# git clone https://github.com/neruthes/os-install
 OSIVER="v0.1.1"
 TARBALURL="https://github.com/neruthes/os-install/archive/$OSIVER.tar.gz"
 
 # Download
 if [[ x`which wget 2> /dev/null` == x/* ]]; then
     wget "$TARBALURL" -O osi-install.tar.gz
+    isERR=0
 elif [[ x`which curl 2> /dev/null` == x/* ]]; then
     curl "$TARBALURL" -o osi-install.tar.gz
-elif
-    echo "Error: Cannot find a downloader (wget or curl). Exiting."
-    exit 1
+    isERR=0
 fi
 
+if [[ isERR == 1 ]]; then
+    echo "Error: Cannot find a downloader (wget or curl). Exiting."
+
+    exit 1
+fi
 
 tar -pxf os-install.tar.gz
 mv os-install*/* .
