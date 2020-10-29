6e0aabe8c528413007dd49eaefeeace6681df6a1
Neruthes <i@neruthes.xyz>
2020-10-29 03:30:30 +0000

    Regular update 2020-10-29T02:38:12-00:00

--- install.sh
+++ install.sh
@@ -10,17 +10,20 @@ cd .os-install
 
 # git clone https://github.com/neruthes/os-install
 OSIVER="v0.1.1"
+TARBALURL="https://github.com/neruthes/os-install/archive/$OSIVER.tar.gz"
 
 # Download
 if [[ x`which wget 2> /dev/null` == x/* ]]; then
-    # Found wget
-    wget -O osi-install.tar.gz "https://github.com/neruthes/os-install/archive/$OSIVER.tar.gz"
+    wget "$TARBALURL" -O osi-install.tar.gz
 elif [[ x`which curl 2> /dev/null` == x/* ]]; then
-    #statements
-    curl "https://github.com/neruthes/os-install/archive/$OSIVER.tar.gz" -o osi-install.tar.gz
+    curl "$TARBALURL" -o osi-install.tar.gz
+elif
+    echo "Error: Cannot find a downloader (wget or curl). Exiting."
+    exit 1
 fi
 
 
-tar -pxf *.tar.gz
+tar -pxf os-install.tar.gz
 mv os-install*/* .
+rm -r os-install*
 source /root/.os-install/src/stage2.sh
